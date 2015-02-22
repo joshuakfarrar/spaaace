c257e1c2d5598607930235e982855b1b121d6429
Joshua K. Farrar <joshua@sent1nel.me>
2015-02-22 16:18:05 -0500

    a _MAX_CHILDREN of 1 is kind of useless

--- client/js/physics/quadtree.js
+++ client/js/physics/quadtree.js
@@ -38,7 +38,7 @@ define(function() {
   var Node = Class.extend({
     init: function(bounds, depth) {
       this._MAX_DEPTH    = 8;
-      this._MAX_CHILDREN = 1;
+      this._MAX_CHILDREN = 2;
 
       this.TOP_LEFT      = 0;
       this.TOP_RIGHT     = 1;
